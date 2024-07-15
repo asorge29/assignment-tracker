0957aef9241312a245050343de3836e33727ed18
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-07-15 16:55:12 -0700

    test

--- pages/api/getItems.js
+++ pages/api/getItems.js
@@ -1,21 +1,23 @@
 // pages/api/getItems.js
-import { DocumentClient } from '../../lib/dynamodb';
+import { DocumentClient } from "../../lib/dynamodb";
 
-export const runtime = 'edge';
+export const config = {
+  runtime: "edge",
+};
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     try {
       const params = {
-        TableName: 'assignmentTrackerAssignments',
+        TableName: "assignmentTrackerAssignments",
       };
       const data = await DocumentClient.scan(params).promise();
       res.status(200).json(data.Items);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Failed to fetch items' });
+      res.status(500).json({ error: "Failed to fetch items" });
     }
   } else {
-    res.status(405).json({ error: 'Method not allowed' });
+    res.status(405).json({ error: "Method not allowed" });
   }
 }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,14 +1,13 @@
 // pages/api/hello.js
 export const config = {
-    runtime: 'edge', // This enables the Edge Runtime for this API route
-  };
-  
-  export default async function handler(req) {
-    return new Response(JSON.stringify({ message: 'Hello from the Edge!' }), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
-  
+  runtime: "edge", // This enables the Edge Runtime for this API route
+};
+
+export default async function handler(req) {
+  return new Response(JSON.stringify({ message: "Hello from the Edge!" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
