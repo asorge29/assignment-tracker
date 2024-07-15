37860ea3f441ea2d05aa5b349ccaa5daf94155fd
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-07-15 16:49:48 -0700

    test edge api route

---
+++ pages/api/hello.js
@@ -0,0 +1,14 @@
+// pages/api/hello.js
+export const config = {
+    runtime: 'edge', // This enables the Edge Runtime for this API route
+  };
+  
+  export default async function handler(req) {
+    return new Response(JSON.stringify({ message: 'Hello from the Edge!' }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -14,34 +14,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
-        <style jsx>{`
-          td, th {
-            border: 1px solid black;
-          }
-          table {
-            border-collapse: collapse;
-          }
-        `}</style>        <h1>Assignment Tracker</h1>
-        <table>
-          <tr>
-            <th>Title</th>
-            <th>Priority</th>
-            <th>Link</th>
-            <th>Due Date</th>
-            <th>Time Estimate</th>
-            <th>Done</th>
-            <th>Overdue</th>
-          </tr>
-          <tr>
-            <td>Assignment 1</td>
-            <td>Assignment 1</td>
-            <td>Assignment 1</td>
-            <td>Assignment 1</td>
-            <td>Assignment 1</td>
-            <td>Assignment 1</td>
-            <td>Assignment 1</td>
-          </tr>
-        </table>
       </body>
     </>
   );
