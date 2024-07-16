044e55735620a97895d18b17e2c6479a80eb3a55
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-07-15 19:47:53 -0700

    ugh test

---
+++ pages/ugh.js
@@ -0,0 +1,19 @@
+export async function getServerSideProps() {
+  const protocol = process.env.VERCEL_ENV === 'production' ? 'https' : 'http';
+  const host = process.env.VERCEL_URL || 'localhost:3000';
+
+  return {
+    props: {
+      protocol: protocol,
+      host: host,
+    },
+  };
+}
+
+export default function Home({ protocol, host }) {
+  return (
+    <div>
+      <h1>{host}</h1>
+    </div>
+  );
+}
