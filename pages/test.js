4a67bb29b01278d9a4be67e3991d813f6e594f87
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-07-15 21:21:50 -0700

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,7 @@
 // pages/index.js
 export async function getServerSideProps() {
   const protocol = process.env.VERCEL_ENV === 'production' ? 'https' : 'http';
-  const host = process.env.VERCEL_URL || 'localhost:3000';
+  const host = process.env.VERCELPROJECT_PRODUCTION_URL || 'localhost:3000';
   const res = await fetch(`${protocol}://${host}/api/getItems`);
   const items = await res.json();
 
