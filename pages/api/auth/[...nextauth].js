a6e7f1742b6e3acfb6b0c4b1ee1c89335d603fd7
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-07-24 19:57:20 -0700

    edge runtime

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,8 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 
+export const runtime = 'edge';
+
 export const authOptions = {
   providers: [
     GithubProvider({
