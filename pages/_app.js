d44fa325941ff2bb0480da60d89f66241bb351b4
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-07-25 23:15:04 -0700

    update deps and remove vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import "@/styles/globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
-      <SpeedInsights />
     </SessionProvider>
   );
 }
