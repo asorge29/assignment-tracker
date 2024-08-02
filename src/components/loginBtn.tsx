fbce0c87ad42063d298d59078d18ce61dd2a1b55
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-08-01 23:07:36 -0700

    better login button

--- src/components/loginBtn.tsx
+++ src/components/loginBtn.tsx
@@ -3,13 +3,23 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function LoginBtn() {
-  const { data: session } = useSession()
+  const { data: session, status } = useSession()
+
+  if (status === "loading") {
+    return (
+      <p>Loading...</p>
+    )
+  }
+
   if (!session) {
     return (
       <button onClick={() => signIn()}>Sign in</button>
     )
   }
   return (
+    <>
     <pre>{JSON.stringify(session)}</pre>
+    <button onClick={() => signOut()}>Sign out</button>
+    </>
   )
 }
