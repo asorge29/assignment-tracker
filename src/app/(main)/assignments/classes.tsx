31672d417486cb2b140da340c83206d7a4a9dd70
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-10-28 21:38:32 -0700

    funky button fix

--- src/app/(main)/assignments/classes.tsx
+++ src/app/(main)/assignments/classes.tsx
@@ -9,10 +9,20 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog"
 import {useSession} from "next-auth/react";
 import {Class} from "@/types/class";
 import {useClassesContext, useAssignmentsContext} from "@/app/(main)/assignments/context";
 import {Assignment} from "@/types/assignment";
+import { Button } from "@/components/ui/button";
 
 export default function Classes() {
   const {data: session, status} = useSession();
@@ -41,6 +51,30 @@ export default function Classes() {
           ))}
         </TableBody>
       </Table>
+      <div>
+        <Dialog>
+          <DialogTrigger>
+            Create
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create a new class</DialogTitle>
+            </DialogHeader>
+            <form>
+              <label htmlFor="name">Class Name</label>
+              <input type="text" id="name" name="name" />
+              <DialogClose asChild>
+                <Button type="submit" className='hover:bg-green-700'>Add Assignment</Button>
+              </DialogClose>
+            </form>
+          </DialogContent>
+        </Dialog>
+        <Dialog>
+          <DialogTrigger className="bg-slate-500 hover:bg-slate-600">
+            Delete
+          </DialogTrigger>
+        </Dialog>
+      </div>
     </div>
   )
 }
