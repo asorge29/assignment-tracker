3a8a994cfc39e1f08aa8df11f8053246ab1e16db
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-10-28 21:53:31 -0700

    fix dialog trigger buttons, finish dialog structure

--- src/app/(main)/assignments/classes.tsx
+++ src/app/(main)/assignments/classes.tsx
@@ -23,17 +23,23 @@ import {Class} from "@/types/class";
 import {useClassesContext, useAssignmentsContext} from "@/app/(main)/assignments/context";
 import {Assignment} from "@/types/assignment";
 import { Button } from "@/components/ui/button";
+import {useEffect, useState} from "react";
 
 export default function Classes() {
   const {data: session, status} = useSession();
   const {classes, setClasses, refetchClasses} = useClassesContext()
   const {assignments, setAssignments, refetchAssignments} = useAssignmentsContext()
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const countAssignments = (classId: number): string => {
     return assignments.filter((assignment: Assignment) => assignment.class === classId).length.toString()
   }
 
-  return (
+  return isClient ? (
     <div>
       <Table>
         <TableHeader>
@@ -51,30 +57,50 @@ export default function Classes() {
           ))}
         </TableBody>
       </Table>
-      <div>
+      <div className='flex flex-row justify-between'>
         <Dialog>
           <DialogTrigger>
-            Create
+            <Button>Create Class</Button>
           </DialogTrigger>
           <DialogContent>
             <DialogHeader>
               <DialogTitle>Create a new class</DialogTitle>
             </DialogHeader>
-            <form>
-              <label htmlFor="name">Class Name</label>
-              <input type="text" id="name" name="name" />
+            <form className='flex flex-col gap-4'>
+              <div className="flex flex-row justify-between">
+                <label htmlFor="name">Class Name</label>
+                <input type="text" id="name" name="name"/>
+              </div>
               <DialogClose asChild>
-                <Button type="submit" className='hover:bg-green-700'>Add Assignment</Button>
+                <Button type="submit" className='hover:bg-green-700 w-full'>Create Class</Button>
               </DialogClose>
             </form>
           </DialogContent>
         </Dialog>
         <Dialog>
-          <DialogTrigger className="bg-slate-500 hover:bg-slate-600">
-            Delete
+          <DialogTrigger>
+            <Button>Delete Class</Button>
           </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Delete a class</DialogTitle>
+            </DialogHeader>
+            <form className='flex flex-col gap-4'>
+              <div className="flex flex-row justify-between">
+                <label htmlFor="name">Class Name</label>
+                <select>
+                  {classes.map((item: Class) => (
+                    <option key={item.id} value={item.id}>{item.name}</option>
+                  ))}
+                </select>
+              </div>
+              <DialogClose asChild>
+                <Button type="submit" className='hover:bg-green-700 w-full'>Create Class</Button>
+              </DialogClose>
+            </form>
+          </DialogContent>
         </Dialog>
       </div>
     </div>
-  )
+  ) : null;
 }
