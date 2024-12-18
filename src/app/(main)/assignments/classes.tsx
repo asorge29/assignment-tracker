c536e958b26e823a68e548aaa94d4ead75ad58a3
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-12-17 21:42:39 -0800

    Merge pull request #11
    * Revamp create class form to use zod for form handling and validation

--- src/app/(main)/assignments/classes.tsx
+++ src/app/(main)/assignments/classes.tsx
@@ -18,6 +18,16 @@ import {
   DialogTrigger,
   DialogClose,
 } from "@/components/ui/dialog"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import {useSession} from "next-auth/react";
 import {Class} from "@/types/class";
 import {useClassesContext, useAssignmentsContext} from "@/app/(main)/assignments/context";
@@ -26,28 +36,41 @@ import { Button } from "@/components/ui/button";
 import {useEffect, useState} from "react";
 import { createClass } from "@/lib/createClass";
 import { deleteClass } from "@/lib/deleteClass";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+const createClassSchema = z.object({
+  name: z.string().min(2, {
+    message: "Class name must be at least 2 characters.",
+  }),
+  email: z.string(),
+})
 
 export default function Classes() {
   const {data: session, status} = useSession();
   const {classes, setClasses, refetchClasses} = useClassesContext()
   const {assignments, setAssignments, refetchAssignments} = useAssignmentsContext()
   const [isClient, setIsClient] = useState(false);
+  const [createFormOpen, setCreateFormOpen] = useState(false);
 
   useEffect(() => {
     setIsClient(true);
   }, []);
 
-  const handleCreateClass = async (e : React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const name = formData.get('name') as string;
-    const email = session?.user?.email;
-    
-    if (name && email) {
-      createClass({ name, email }).then(() => {
-        refetchClasses();
-      });
+  const createForm = useForm<z.infer<typeof createClassSchema>>({
+    resolver: zodResolver(createClassSchema),
+    defaultValues: {
+      name: "",
+      email: session?.user?.email as string,
     }
+  })
+
+  function createClassSubmit(values: z.infer<typeof createClassSchema>) {
+    createClass(values).then(() => {
+      refetchClasses();
+    });
+    setCreateFormOpen(false);
   }
 
   const handleDeleteClass = async (e : React.FormEvent<HTMLFormElement>) => {
@@ -86,7 +109,7 @@ export default function Classes() {
         </TableBody>
       </Table>
       <div className='flex flex-row justify-between'>
-        <Dialog>
+        <Dialog open={createFormOpen} onOpenChange={setCreateFormOpen}>
           <DialogTrigger>
             <Button>Create Class</Button>
           </DialogTrigger>
@@ -94,15 +117,24 @@ export default function Classes() {
             <DialogHeader>
               <DialogTitle>Create a new class</DialogTitle>
             </DialogHeader>
-            <form className='flex flex-col gap-4' onSubmit={handleCreateClass}>
-              <div className="flex flex-row gap-4 justify-center items-center">
-                <label className='text-lg' htmlFor="name">Class Name:</label>
-                <input type="text" id="name" name="name" className='rounded text-lg border p-0.5'/>
-              </div>
-              <DialogClose asChild>
+            <Form {...createForm}>
+              <form className='flex flex-col gap-4' onSubmit={createForm.handleSubmit(createClassSubmit)}>
+              <FormField
+                control={createForm.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Class Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Calculus AB" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
                 <Button type="submit" className='hover:bg-green-700 w-full'>Create Class</Button>
-              </DialogClose>
-            </form>
+              </form>
+            </Form>
           </DialogContent>
         </Dialog>
         <Dialog>
