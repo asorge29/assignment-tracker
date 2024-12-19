4fd27d9de462ee5ac0f3acc0709b8fcf207ef88c
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-12-18 22:12:20 -0800

    Patch header scrolling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,8 @@ export default async function Header({}) {
   return (
     <div className="w-full flex flex-col md:flex-row justify-between items-center p-4 border-b">
       <h1 className="md:text-4xl text-xl font-semibold">Assignment Tracker</h1>
-      <div className="flex gap-4 justify-center items-center overflow-scroll w-full md:w-auto">
+      {/* TODO: Redo this using dialog component for profile, scrolling is janky too */}
+      <div className="flex gap-4 justify-center items-center overflow-scroll w-full md:overflow-visible md:w-auto">
         <Link href="/" className="md:text-2xl text-lg hover:underline">Home</Link>
         <Link href="/about" className="md:text-2xl text-lg hover:underline">About</Link>
         <Link href="/assignments" className="md:text-2xl text-lg hover:underline">Assignments</Link>
