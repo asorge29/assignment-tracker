4bf5830dd0e2d2eb993fc4e23328a58af83f3ff2
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-12-19 16:16:53 -0800

    redo text inputs with zod

--- src/components/newAssignment.tsx
+++ src/components/newAssignment.tsx
@@ -9,27 +9,54 @@ import {
   DialogTrigger,
   DialogClose,
 } from "./ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import {Input} from "@/components/ui/input"
 import {Button} from "@/components/ui/button";
 import {Class} from "@/types/class";
 import React from "react";
 import { createAssignment } from "@/lib/createAssignment";
+import { z } from "zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  title: z.string(),
+  link: z.string().url(),
+  dueDate: z.string(),
+  className: z.string(),
+  email: z.string().email(),
+})
 
 export default function NewAssignment({ classes, session, refetchAssignments}: { classes: Class[], session: any, refetchAssignments: () => void }) {
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const title = formData.get("title") as string;
-    const link = formData.get("link") as string;
-    const dueDate = formData.get("dueDate") as string;
-    const className = formData.get("class") as string;
-    const email = session.user?.email;
-
-    console.log(title, link, dueDate, className, email);
-
-    await createAssignment({ title, link, dueDate, className, email })
-    
-    refetchAssignments();
+  const handleSubmit = (values) => {
+    console.log(values)
   };
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      link: "",
+      dueDate: "",
+      className: "",
+      email: session?.user?.email,
+    }
+  })
 
   return (
     <Dialog>
@@ -40,31 +67,70 @@ export default function NewAssignment({ classes, session, refetchAssignments}: {
         <DialogHeader>
           <DialogTitle>Create Assignment</DialogTitle>
         </DialogHeader>
-        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-          <div className='flex flex-row gap-4 justify-center items-center'>
-            <label htmlFor="title" className='text-lg'>Title:</label>
-            <input type="text" id="title" name="title" required className='rounded text-lg border p-0.5'/>
-          </div>
-          <div className='flex flex-row gap-4 justify-center items-center'>
-            <label htmlFor="link" className='text-lg'>Link:</label>
-            <input type="url" id="link" name="link" className='rounded text-lg border p-0.5'/>
-          </div>
-          <div className='flex flex-row gap-4 justify-center items-center'>
-            <label htmlFor="dueDate" className='text-lg'>Due Date:</label>
-            <input type="date" id="dueDate" name="dueDate" required className='rounded text-lg border p-0.5'/>
-          </div>
-          <div className='flex flex-row gap-4 justify-center items-center'>
-            <label htmlFor="class" className='text-lg'>Class:</label>
-            <select id="class" name="class" className='rounded text-lg border p-0.5'>
-              {classes.map((item, index) => (
-                <option key={index} value={item.id}>{item.name}</option>
-              ))}
-            </select>
-          </div>
-          <DialogClose asChild>
-            <Button type="submit" className='hover:bg-green-700'>Add Assignment</Button>
-          </DialogClose>
-        </form>
+        <Form {...form}>
+        <form className='flex flex-col gap-4' onSubmit={form.handleSubmit(handleSubmit)}>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Topic Article #4" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="link"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Link</FormLabel>
+                <FormControl>
+                  <Input placeholder="https://docs.google.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="dueDate"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Due Date</FormLabel>
+                <FormControl>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="className"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Class</FormLabel>
+                <Select>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a class for this assignment."/>
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {classes.map((classItem, index) => (
+                      <SelectItem key={index} value={JSON.stringify(classItem.id)}>{classItem.name}</SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+            <Button type="submit" className='hover:bg-green-700'>Create Assignment</Button>
+        </form></Form>
       </DialogContent>
     </Dialog>
   );
